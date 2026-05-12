b94428000ef88c1eb9f392b3befc3a018a022dad
Sultan Almaghthawi <thesultan.eid@gmail.com>
2026-05-12 17:08:49 +0300

    Delete threatforge directory

--- threatforge/server/src/db/migrate.js
+++
@@ -1,8 +0,0 @@
-import { runSchema, db } from './index.js';
-
-console.log('[migrate] Applying schema…');
-runSchema();
-
-const tables = db.prepare("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name").all();
-console.log('[migrate] Tables present:', tables.map((t) => t.name).join(', '));
-console.log('[migrate] Done.');
